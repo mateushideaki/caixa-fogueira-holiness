d059cdfe487c813c35515fedcab125ef6751a40d
Hideaki <mateustaroda@gmail.com>
2019-05-10 22:09:09 -0300

    Converting to functional

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { positions, Provider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+const options = {
+    timeout: 5000,
+    position: positions.BOTTOM_CENTER
+};
+
+ReactDOM.render(
+    <Provider template={AlertTemplate} {...options}>
+        <App />
+    </Provider>, document.getElementById('root'));
--- src/pages/caixa/index.js
+++ src/pages/caixa/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
+import { useAlert } from "react-alert";
+
 import './styles.css';
 
-export default class caixa extends Component {
+const Caixa = () => {
 
-    state = {
+    const alert = useAlert();
+
+    const [state, setState] = React.useState({
+        troco: 0,
         itens: [],
         total: 0,
+        pagamento: 0,
         produtos: [
             { nome: 'Espetinho', valor: 3.50, cor: 'rgb(154, 246, 0)' },
             { nome: 'Yakissoba', valor: 15.00, cor: 'rgb(246, 0, 68)' },
@@ -18,14 +24,14 @@ export default class caixa extends Component {
             { nome: 'Yakissoba', valor: 15.00, cor: 'rgb(246, 0, 68)' },
             { nome: 'Refrigerante', valor: 3.50, cor: 'rgb(0, 167, 246)' }
         ]
-    }
+    });
 
-    adicionaProduto = (produto) => {
-        const itens = this.state.itens;
+    const adicionaProduto = (produto) => {
+        const itens = state.itens;
 
         let indexProdutoAdicionado = itens.findIndex(produtoAtual => produtoAtual.nome === produto.nome);
         let produtoAdicionado;
-        let total = this.state.total + produto.valor;
+        let total = state.total + produto.valor;
 
         if (indexProdutoAdicionado !== -1) {
             produtoAdicionado = itens[indexProdutoAdicionado];
@@ -35,42 +41,71 @@ export default class caixa extends Component {
             produtoAdicionado = { ...produto, quantidade: 1 };
             itens.push(produtoAdicionado);
         }
-        const produtos = this.state.produtos;
-        this.setState({ produtos, total, itens });
-        console.log(this.state);
+        const produtos = state.produtos;
+        setState({ ...state, produtos, total, itens });
     }
 
-    render() {
-        return (
-            <div id="caixa-container">
-                <section>
-                    {this.state.produtos.map((produto, index) => (
-                        <div onClick={() => this.adicionaProduto(produto)} key={index} style={{ backgroundColor: produto.cor }}>
-                            {`${produto.nome} R$ ${produto.valor}`}
+    const formataValor = (valor) => {
+        const format = { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' };
+        return valor.toLocaleString('pt-BR', format);
+    }
+
+    const calculaTroco = () => {
+        let troco = state.pagamento - state.total;
+
+        if (troco < 0) {
+            alert.error("O Valor do pagamento deve ser maior que o valor total do pedido");
+            return;
+        }
+        setState({ ...state, troco });
+    }
+
+    const handleChange = (e) => {
+        setState({ ...state, pagamento: e.target.value });
+    }
+
+    const limparTela = () => {
+        setState({ ...state, troco: 0, itens: [], total: 0, pagamento: 0 });
+    }
+
+    return (
+        <div id="caixa-container">
+            <section>
+                {state.produtos.map((produto, index) => (
+                    <div className="ficha" onClick={() => adicionaProduto(produto)} key={index} style={{ backgroundColor: produto.cor }}>
+                        <div>
+                            {produto.nome}
                         </div>
-                    ))}
-                </section>
-
-                <div id="container-pagamento">
-                    <div>
-                        <input placeholder="Valor pagamento"></input>
-                        <button>Calcular Troco</button>
+                        <div>
+                            {formataValor(produto.valor)}
+                        </div>
                     </div>
-                    <h3>Troco: R$15,57</h3>
+                ))}
+            </section>
+
+            <div id="container-pagamento">
+                <div>
+                    <input value={state.pagamento} onChange={handleChange.bind(this)} placeholder="Valor pagamento"></input>
+                    <button className="btn-troco" onClick={() => calculaTroco()}>Calcular Troco</button>
+                    <button className="btn-limpar" onClick={() => limparTela()}>Limpar tela</button>
                 </div>
-
-                <div className="pedido">
-                    <h3>Pedido atual</h3>
-                    <ul>
-                        {this.state.itens.map((item, index) => (
-                            <li key={index}>{`${item.nome} (${item.quantidade})`}</li>
-                        ))}
-                    </ul>
-
-                    <h4>{`Total: R$${this.state.total}`}</h4>
-
-                </div>
+                <h3>Troco: {formataValor(state.troco)}</h3>
             </div>
-        );
-    }
+
+            <div className="pedido">
+                <h3>Pedido atual</h3>
+                <ul>
+                    {state.itens.map((item, index) => (
+                        <li key={index}>{`${item.nome} (${item.quantidade})`}</li>
+                    ))}
+                </ul>
+
+                <h4>{`Total: ${formataValor(state.total)}`}</h4>
+
+            </div>
+        </div>
+    );
+
 }
+
+export default Caixa;
